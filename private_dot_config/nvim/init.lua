edb94d2e4352a0bf4c17eef684c6ae1b6930abd8
yuguorui <yuguorui@pku.edu.cn>
2024-06-07 14:02:23 +0800

    auto close nvim-tree and outline before saving the session

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -319,23 +319,16 @@ require("lazy").setup({
         "mhinz/vim-startify",
     },
     {
-        "pysan3/autosession.nvim", -- restore previous session
-        event = { "VeryLazy" },                   -- OPTIONAL
-        -- lazy = false,                          -- If you do not want to lazy load.
-        dependencies = { "mhinz/vim-startify" },  -- OPTIONAL: Used for `:AutoSessionGlobal`
-        config = function(_, opts)
-            require("autosession").setup({
-                msg = nil, -- string: printed when startup is completed
-                restore_on_setup = true, -- boolean: If true, automatically restore session on nvim startup
-                warn_on_setup = false, -- boolean: If true, warning shown when no `.session.vim` is found
-                autosave_on_quit = true, -- boolean: If true, automatically overwrites sessionfile if exists
-                save_session_global_dir = vim.g.startify_session_dir or vim.fn.stdpath("data") .. "/session", -- string
-                --                        dir path to where global session files should be stored.
-                --                        global sessions will show up in startify screen as dirname of the session
-                sessionfile_name = ".session.vim", -- string: default name of sessionfile. better be .gitignored
-                disable_envvar = "NVIM_DISABLE_AUTOSESSION", -- string: disable this plugin altogether when this envvar is found
-            })
-       end
+        'rmagatti/auto-session',
+        config = true,
+        opts = {
+            pre_save_cmds = {
+                function()
+                    require"nvim-tree.api".tree.close()
+                    require"outline".close()
+                end
+            },
+        }
     },
     {
         "shellRaining/hlchunk.nvim",
