46e561800e0a946bd67f8be61b7e8754d27865c6
yuguorui <yuguorui@pku.edu.cn>
2024-08-14 10:11:03 +0800

    plug: use gitsigns to replace vim-gitgutter
    gitsigns is faster than gitgutter, and gitgutter has a bug that show
    a whole file as new added.
    
    Signed-off-by: yuguorui <yuguorui@pku.edu.cn>

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -181,7 +181,55 @@ require("lazy").setup({
         end
     },
     {
-        'airblade/vim-gitgutter',
+        "lewis6991/gitsigns.nvim",
+        config = function()
+            require("gitsigns").setup{
+                on_attach = function(bufnr)
+                    local gitsigns = require('gitsigns')
+
+                    local function map(mode, l, r, opts)
+                        opts = opts or {}
+                        opts.buffer = bufnr
+                        vim.keymap.set(mode, l, r, opts)
+                    end
+
+                    -- Navigation
+                    map('n', ']c', function()
+                        if vim.wo.diff then
+                         vim.cmd.normal({']c', bang = true})
+                        else
+                            gitsigns.next_hunk()
+                        end
+	            end)
+
+                    map('n', '[c', function()
+                        if vim.wo.diff then
+                            vim.cmd.normal({'[c', bang = true})
+                        else
+                            gitsigns.prev_hunk()
+                        end
+                    end)
+
+                    -- Actions
+                    map('n', '<leader>hs', gitsigns.stage_hunk)
+                    map('n', '<leader>hr', gitsigns.reset_hunk)
+                    map('v', '<leader>hs', function() gitsigns.stage_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                    map('v', '<leader>hr', function() gitsigns.reset_hunk {vim.fn.line('.'), vim.fn.line('v')} end)
+                    map('n', '<leader>hS', gitsigns.stage_buffer)
+                    map('n', '<leader>hu', gitsigns.undo_stage_hunk)
+                    map('n', '<leader>hR', gitsigns.reset_buffer)
+                    map('n', '<leader>hp', gitsigns.preview_hunk)
+                    map('n', '<leader>hb', function() gitsigns.blame_line{full=true} end)
+                    map('n', '<leader>tb', gitsigns.toggle_current_line_blame)
+                    map('n', '<leader>hd', gitsigns.diffthis)
+                    map('n', '<leader>hD', function() gitsigns.diffthis('~') end)
+                    map('n', '<leader>td', gitsigns.toggle_deleted)
+
+                    -- Text object
+                    map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+                end
+            }
+        end,
     },
     {
         "nvim-treesitter/nvim-treesitter",
