34f80d9423bed8cc4910711b173edca1096dc631
yuguorui <yuguorui@pku.edu.cn>
2026-02-26 14:45:39 +0800

    Make sure nvim cursor visibility
    Signed-off-by: yuguorui <yuguorui@pku.edu.cn>

--- private_dot_config/nvim/init.lua
+++ private_dot_config/nvim/init.lua
@@ -770,3 +770,8 @@ keyset("n", "<leader>n", ":Telekasten<CR>", opts)
 
 -- markdown
 vim.g.markdown_fenced_languages = { "javascript", "bash", "diff", "go", "rust", "c" }
+
+-- make sure cursor visible
+vim.opt.guicursor = "n-v-c:block,i-ci-ve:ver25,r-cr:hor20,o:hor50"
+  .. ",a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor"
+  .. ",sm:block-blinkwait175-blinkoff150-blinkon175"
